91a1d70cc3202c705bf7690e7665837ea36c3294
kavidey <kaviasher@gmail.com>
2024-07-04 15:18:07 +0800

    Fix NSD voxel size

--- code/sc_mbm/utils.py
+++ code/sc_mbm/utils.py
@@ -65,6 +65,27 @@ def interpolate_pos_embed(model, checkpoint_model):
             pos_tokens = pos_tokens.permute(0, 2, 1)
             new_pos_embed = torch.cat((extra_tokens, pos_tokens), dim=1)
             checkpoint_model['pos_embed'] = new_pos_embed
+    if 'decoder_pos_embed':
+        pos_embed_checkpoint = checkpoint_model['decoder_pos_embed']
+        embedding_size = pos_embed_checkpoint.shape[-1]
+        num_patches = model.patch_embed.num_patches
+        num_extra_tokens = model.pos_embed.shape[-2] - num_patches # cls token
+        # height (== width) for the checkpoint position embedding
+        orig_size = int(pos_embed_checkpoint.shape[-2] - num_extra_tokens)
+        # height (== width) for the new position embedding
+        new_size = int(num_patches)
+        # class_token and dist_token are kept unchanged
+        if orig_size != new_size:
+            print("Decoder Position interpolate from %d to %d" % (orig_size, new_size))
+            extra_tokens = pos_embed_checkpoint[:, :num_extra_tokens]
+            # only the position tokens are interpolated
+            pos_tokens = pos_embed_checkpoint[:, num_extra_tokens:]
+            pos_tokens = pos_tokens.reshape(-1, orig_size, embedding_size).permute(0, 2, 1)
+            pos_tokens = torch.nn.functional.interpolate(
+                pos_tokens, size=(new_size))
+            pos_tokens = pos_tokens.permute(0, 2, 1)
+            new_pos_embed = torch.cat((extra_tokens, pos_tokens), dim=1)
+            checkpoint_model['decoder_pos_embed'] = new_pos_embed
 
 
 
