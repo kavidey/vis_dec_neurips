21ccea0375cf6a03234e1acc5aff02e9eccfab20
kavidey <kaviasher@gmail.com>
2024-07-08 08:34:45 +0800

    Clean up eval code

--- code/eval.py
+++ code/eval.py
@@ -13,6 +13,7 @@ import numpy as np
 import copy
 import argparse
 from sklearn.metrics.pairwise import cosine_similarity
+from tqdm import tqdm
 
 import torch
 from torch.utils.data import DataLoader
@@ -39,8 +40,8 @@ from transformers.models.clip.modeling_clip import clip_loss
 os.environ["WANDB_START_METHOD"] = "thread"
 os.environ["WANDB_DIR"] = "."
 # %%
-model_ckpt_path = "results/fmri_finetune_GOD_sbj_1/27-06-2024-09-50-45/checkpoints_300/checkpoint_singlesub_clip_cross_att_GOD_sbj_1_fmriw0.25_imgw1.5_fmar0.75_imar0.5_fmridl6_imgdl6_pretr1_with_checkpoints_pre_140_doublecontra.pth_epo300_mergconf.pth"
-model_image_ckpt_path = "results/fmri_finetune_GOD_sbj_1/27-06-2024-09-50-45/checkpoints_300/checkpoint_singlesub_clip_cross_att_GOD_sbj_1_fmriw0.25_imgw1.5_fmar0.75_imar0.5_fmridl6_imgdl6_pretr1_with_checkpoints_pre_140_doublecontra.pth_epo300_mergconf_img.pth"
+model_ckpt_path = "results/fmri_finetune_NSD_[1]/04-07-2024-16-12-54/checkpoints_50/checkpoint_singlesub_clip_cross_att_NSD_[1]_fmriw0.25_imgw1.5_fmar0.75_imar0.5_fmridl6_imgdl6_pretr1_with_checkpoints_pre_140_doublecontra.pth_epo50_mergconf.pth"
+model_image_ckpt_path = "results/fmri_finetune_NSD_[1]/04-07-2024-16-12-54/checkpoints_50/checkpoint_singlesub_clip_cross_att_NSD_[1]_fmriw0.25_imgw1.5_fmar0.75_imar0.5_fmridl6_imgdl6_pretr1_with_checkpoints_pre_140_doublecontra.pth_epo50_mergconf_img.pth"
 # %%
 config = Config_MBM_finetune_cross()
 # testing arguments
@@ -54,8 +55,11 @@ config.img_mask_ratio = 0.5
 config.fmri_recon_weight = 0.25
 config.mask_ratio = 0.75
 config.dataset = "NSD"
-config.batch_size = 4
+config.batch_size = 10
 config.img_ca_weight = 1
+config.img_skip_weight = 0
+config.fmri_ca_weight = 1
+config.fmri_skip_weight = 1
 config.guidance_scale = 1
 
 sd = torch.load(config.pretrain_mbm_path, map_location="cpu")
@@ -77,11 +81,19 @@ model_image_config.num_attention_heads = 16
 
 # round number of voxels in NSD to multiple of 16
 if config.dataset == "NSD":
-    num_voxels = (15724//16)*16
+    num_voxels = (15724 // 16) * 16
 else:
     num_voxels = None
 
-model = fMRICLIPAutoEncoder(config, model_image_config, config.clip_dim, ca_weight=config.fmri_ca_weight, skip_weight=config.fmri_skip_weight, num_voxels=num_voxels, device=device)
+model = fMRICLIPAutoEncoder(
+    config,
+    model_image_config,
+    config.clip_dim,
+    ca_weight=config.fmri_ca_weight,
+    skip_weight=config.fmri_skip_weight,
+    num_voxels=num_voxels,
+    device=device,
+)
 model.load_state_dict(torch.load(model_ckpt_path)["model"])
 model.eval()
 model.to(device)
@@ -90,8 +102,9 @@ num_voxels = model.num_voxels
 model_image = ConditionLDM(
     model_image_config,
     config.clip_dim,
-    config.img_ca_weight,
-    config.guidance_scale,
+    ca_weight=config.img_ca_weight,
+    skip_weight=config.img_skip_weight,
+    guidance_scale=config.guidance_scale,
     device=device,
 )
 model_image.load_state_dict(torch.load(model_image_ckpt_path))
@@ -122,7 +135,7 @@ elif config.dataset == "NSD":
         patch_size=config_pretrain.patch_size,
         fmri_transform=torch.FloatTensor,
         subjects=config.nsd_subs,
-        include_non_avg_test=config.include_nonavg_test
+        include_non_avg_test=config.include_nonavg_test,
     )
 else:
     raise NotImplementedError
@@ -152,7 +165,9 @@ else:
 all_clip_embeddings = []
 all_fmri_embeddings = []
 
-for data_dcit in dataloader_hcp_test:
+for data_iter_step, data_dcit in tqdm(
+    enumerate(dataloader_hcp_test), leave=False, total=test_set.num_items
+):
     if config.dataset != "NSD":
         samples = data_dcit["fmri"]
 
@@ -162,6 +177,9 @@ for data_dcit in dataloader_hcp_test:
         image_class = data_dcit["image_class"].to(device)
     else:
         images, samples = test_set.get_batch(data_dcit)
+
+    images = images.to(device)
+    samples = samples.to(device)
 
     with torch.no_grad() and torch.cuda.amp.autocast(enabled=True):
         image_input = model_image.image_feature_extractor(
@@ -174,28 +192,34 @@ for data_dcit in dataloader_hcp_test:
         # clip_cls = clip_cls.div(torch.norm(clip_cls, p=2, dim=1, keepdim=True))
 
         fmri_embeddings = model(samples, encoder_only=True)
-        fmri_embeddings = model_image.encode_clip(images) + model_image.cross_attention(model_image.encode_clip(images), fmri_embeddings)
-        # fmri_embeddings = model_image.cross_attention(model_image.encode_clip(images), fmri_embeddings)
+        # fmri_embeddings = model_image.encode_clip(images) + model_image.cross_attention(model_image.encode_clip(images), fmri_embeddings)
+        fmri_embeddings = model_image.cross_attention(model_image.encode_clip(images), fmri_embeddings)
         fmri_cls = fmri_embeddings[:, 0]
         # fmri_cls = fmri_cls.div(torch.norm(fmri_cls, p=2, dim=1, keepdim=True))
 
         all_clip_embeddings.extend(clip_cls.detach().cpu())
         all_fmri_embeddings.extend(fmri_cls.detach().cpu())
 
+    if config.dataset == "NSD":
+        if data_iter_step >= test_set.num_items:
+            break
+
 all_clip_embeddings = torch.vstack(all_clip_embeddings)
 all_fmri_embeddings = torch.vstack(all_fmri_embeddings)
 # %%
-# model.ca_weight = 1
-# generated = model_image.generate_image(images, fmri_embeddings, 50)
-# F.to_pil_image(torch.cat([*generated], dim=2))
+num_images = min(16, config.batch_size)
+generated = model_image.generate_image(
+    images[:num_images], fmri_embeddings[:num_images], 50
+)
+F.to_pil_image(torch.cat([*generated], dim=2))
 # %%
-# F.to_pil_image(torch.cat([*images], dim=2))
+F.to_pil_image(torch.cat([*images[:num_images]], dim=2))
 # %%
 clip_scores = torch.nn.functional.cosine_similarity(
     all_clip_embeddings, all_fmri_embeddings, dim=1
 )
 
-print(f"CLIP Score: {torch.mean(clip_scores)}")  # %%
+print(f"Cosine similarity: {torch.mean(clip_scores)}")  # %%
 # %%
 cf_cossim = torch.tensor(cosine_similarity(all_clip_embeddings, all_fmri_embeddings))
 fc_cossim = torch.tensor(cosine_similarity(all_fmri_embeddings, all_clip_embeddings))
@@ -210,12 +234,15 @@ fc_retrieval = torch.sum(torch.argsort(fc_cossim, axis=1)[:, -1] == labels) / le
     labels
 )
 
-print(f"CLIP retrieval from fMRI: {cf_retrieval:.2%}")
-print(f"fMRI retrieval from CLIP: {fc_retrieval:.2%}")
+print(f"CLIP retrieval from fMRI: {cf_retrieval:.3%}")
+print(f"fMRI retrieval from CLIP: {fc_retrieval:.3%}")
+print(f"Random chance: {1/all_clip_embeddings.shape[0]:.3%}")
 # %%
 # logit_scale = model_image.image_encoder.logit_scale.exp()
 logit_scale = torch.tensor(2.6592).exp()
-logits_per_text = torch.matmul(all_clip_embeddings.float(), all_fmri_embeddings.t()) * logit_scale
+logits_per_text = (
+    torch.matmul(all_clip_embeddings.float(), all_fmri_embeddings.t()) * logit_scale
+)
 logits_per_image = logits_per_text.t()
 clip_loss(logits_per_text)
 # %%
